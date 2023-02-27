19d7576dae0e1c538d08086231af1529f32dc960
NIK0LVI <radicnikola93@hotmail.com>
2023-02-27 18:48:52 +0100

    Init db and api config.

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_HOSTNAME: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    # USER_SECRET: str
+
+    class Config:
+        env_file = "./.env"
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-if __name__ == '__main__':
-    pass
+from app.db.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+
+def init_app():
+    app = FastAPI()
+    return app
+
+
+app = init_app()
+
+
+@app.get("/", include_in_schema=False)
+def hello_world():
+    return RedirectResponse("/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
