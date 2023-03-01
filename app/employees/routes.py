e19342cfb17bf88a6a059e20167ff2a104018983
NIK0LVI <radicnikola93@hotmail.com>
2023-03-01 23:29:07 +0100

    Employee routes.

--- app/employees/routes.py
+++ app/employees/routes.py
@@ -7,8 +7,6 @@ from app.employees.schemas.employee_schema import EmployeeSchema, EmployeeSchema
 
 employee_router = APIRouter(tags=["employees"], prefix="/api/employees")
 
-""" Placeholder for login router. """
-
 
 @employee_router.post("/add-new-employee", response_model=EmployeeSchema)
 def create_employee(employee: EmployeeSchemaIn):
