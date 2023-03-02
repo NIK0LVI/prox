b59a6e3d90e2fa77cc188e32c52bfc13f8c822f3
NIK0LVI <radicnikola93@hotmail.com>
2023-03-02 01:06:46 +0100

    Main reroute.

--- app/main.py
+++ app/main.py
@@ -3,16 +3,18 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
 from app.db.database import Base, engine
-from app.employees.routes import employee_router
 from app.offices.routes import office_router
+from app.employees.routes import employee_router
+from app.students.routes import student_router
 
 Base.metadata.create_all(bind=engine)
 
 
 def init_app():
     app = FastAPI()
-    app.include_router(employee_router)
     app.include_router(office_router)
+    app.include_router(employee_router)
+    app.include_router(student_router)
 
     return app
 
